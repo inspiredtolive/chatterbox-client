664ee96e688f7724df4d0c08fcdfa30d83834660
Marco Chan <inspiredtolive0@gmail.com>
2016-08-08 14:46:42 -0700

    Set up app.js to request and receive messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,9 @@
 // YOUR CODE HERE:
-
+var messages = [];
+$.ajax('https://api.parse.com/1/classes/messages', {
+  datatype: 'json',
+  complete: function (obj, status) {
+    messages = obj.responseJSON.results;
+    console.log(messages);
+  }
+});

2280ab78836b3dd66df51d6524ff6fe8a55846af
inspiredtolive <inspiredtolive0@gmail.com>
2016-08-09 11:49:18 -0700

    Complete min requirements

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,41 +8,62 @@ var username = '';
 var newestDate = {
   'lobby': 0
 };
+var users = {};
+var friends = [];
 var app = {
+  url: 'https://api.parse.com/1/classes/messages',
   init: function () {
+    console.log('initializing');
     $('.submit').on('submit', app.handleSubmit);
-    console.log('fetching')
+    $('#roomSelect').on('change', () => {
+      _.each(newestDate, (item, key) => {
+        newestDate[key] = 0;
+      });
+      app.clearMessages();
+      app.fetch();
+      console.log('changing rooms!');
+    });
     app.fetch();
   },
   send: function (data) {
     $.ajax({
-      url: 'https://api.parse.com/1/classes/messages',
+      url: this.url,
       type: 'POST',
       dataType: 'json',
       data: JSON.stringify(data)
     });
   },
-  fetch: function () {
+  fetch: function (data) {
     $.ajax({
-      url: 'https://api.parse.com/1/classes/messages/',
+      url: this.url,
       datatype: 'json',
       success: function (obj) {
         var currentRoom = $('#roomSelect').val();
+
+        // filters the message room name
+        // add new rooms to roomSelect
+        // adds most recent date to newestDate Obj
         var roomMessages = obj.results.filter((msgObj) => {
-          // adds new room name to room select
-          if (!newestDate.hasOwnProperty(msgObj.roomname)) {
+          if (msgObj.roomname && !newestDate.hasOwnProperty(msgObj.roomname)) {
             $('#roomSelect').append('<option value="' + msgObj.roomname + '">' + msgObj.roomname + '</option>');
-            newestDate[msgObj.roomname] = Date.parse(msgObj.createdAt);
+            newestDate[msgObj.roomname] = 0;
           }
           return msgObj.roomname === currentRoom;
         });
+
+        // add messages 
         var time = Date.parse(roomMessages[0].createdAt);
-        console.log(time, newestDate[currentRoom]);
+        console.log(time, newestDate[currentRoom], newestDate, currentRoom);
         if (time > newestDate[currentRoom]) {
           roomMessages.filter((msgObj) => Date.parse(msgObj.createdAt) > newestDate[currentRoom]).forEach(app.addMessage);
           newestDate[currentRoom] = Date.parse(obj.results[0].createdAt);
         }
 
+        friends.forEach((friend) => {
+          var user = users[friend];
+          console.log(user);
+          $('.' + user).parents('.chat').addClass('friend');
+        });
       //   var time = Date.parse(obj.results[0].createdAt);
       //   if (time > newestDate) {
       //     obj.results.filter((msgObj) => Date.parse(msgObj.createdAt) > newestDate).forEach(app.addMessage);
@@ -51,6 +72,7 @@ var app = {
       }
     });
   },
+
   clearMessages: function () {
     $('#chats').empty();
   },
@@ -59,8 +81,11 @@ var app = {
     _.each(message, (item, key) => {
       saferMessage[key] = _.escape(item);
     });
-    var $message = $('<div class="chat"><span class="username">' + saferMessage.username + ': </span>' + saferMessage.text + '</div>');
-    $message.click(app.addFriend);
+    if (!users.hasOwnProperty(saferMessage.username)) {
+      users[saferMessage.username] = 'user' + Object.keys(users).length;
+    }
+    var $message = $('<div class="chat"><span class="username ' + users[saferMessage.username] + '">' + saferMessage.username + '</span>: ' + saferMessage.text + '</div>');
+    $message.children().click(app.addFriend);
     if (newestDate[$('#roomSelect').val()] === 0) {
       $('#chats').append($message);
     } else {
@@ -70,13 +95,23 @@ var app = {
   addRoom: function (roomName) {
     $('#roomSelect').append('<' + roomName + '></' + roomName + '>');
   },
-  addFriend: function () {},
-  handleSubmit: function () {
+  addFriend: function () {
+    var username = $(this).text();
+    var user = users[username];
+    if (friends.indexOf(username) === -1) {
+      friends.push(username);
+    }
+    $('.' + user).parents('.chat').addClass('friend');
+  },
+  handleSubmit: function (e) {
+    e.preventDefault();
     app.send({
       username: username,
       text: $('#message').val(),
-      roomname: 'lobby'
+      roomname: $('#roomSelect').val()
     });
+    $('#message').val('');
+    app.fetch();
   }
 
 };
@@ -87,6 +122,7 @@ $(function() {
 });
 
 
-// setInterval(() => {
-//   app.fetch();
-// }, 1000);
+setInterval(() => {
+  console.log($('#roomSelect').val());
+  app.fetch();
+}, 10000);
